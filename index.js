da009518129527966bf55a6750e8690db7ba37c7
jackhammer94 <andrewmoses6@gmail.com>
2021-07-02 15:23:42 +0530

    working poc

--- index.js
+++ index.js
@@ -6,126 +6,117 @@ window.onload = function () {
       {
         name: 'testEvent-allday',
         location: 'india',
-        startsOn: {
-          date: 'June 30, 2021',
-          time: '00:00:00'
-        },
-        endsOn: {
-          date: 'June 30, 2021',
-          time: '00:00:00'
-        },
+        startsOn: "",
+        endsOn: "",
         isAllDay: true
       },
       {
         name: 'testEvent-1',
         location: 'india',
-        startsOn: {
-          date: 'June 30, 2021',
-          time: '03:24:00'
-        },
-        endsOn: {
-          date: 'June 30, 2021',
-          time: '03:24:00'
-        },
+        startsOn: "",
+        endsOn: "",
         isAllDay: false
       },
       {
         name: 'testEvent-2',
         location: 'india',
-        startsOn: {
-          date: 'June 30, 2021',
-          time: '03:24:00'
-        },
-        endsOn: {
-          date: 'June 30, 2021',
-          time: '03:24:00'
-        },
+        startsOn: "",
+        endsOn: "",
         isAllDay: false
       },
       {
         name: 'testEvent-overlap-1',
         location: 'india',
-        startsOn: {
-          date: 'June 30, 2021',
-          time: '03:00:00'
-        },
-        endsOn: {
-          date: 'June 30, 2021',
-          time: '04:00:00'
-        },
+        startsOn: "",
+        endsOn: "",
         isAllDay: false
       },
-      // {
-      //   name: 'testEvent-overlap-2',
-      //   location: 'india',
-      //   startsOn: {
-      //     date: 'June 30, 2021',
-      //     time: '03:30:00'
-      //   },
-      //   endsOn: {
-      //     date: 'June 30, 2021',
-      //     time: '04:00:00'
-      //   },
-      //   isAllDay: true
-      // },
-      // {
-      //   name: 'testEvent-overlap-3',
-      //   location: 'india',
-      //   startsOn: {
-      //     date: 'June 30, 2021',
-      //     time: '03:45:00'
-      //   },
-      //   endsOn: {
-      //     date: 'June 30, 2021',
-      //     time: '04:00:00'
-      //   },
-      //   isAllDay: true
-      // }
     ]
   };
   console.log(data)
   const currentTime = new Date("June 30, 2021 00:00:00")
-  const eventMap = {
-    "9": [
+  //TODO: form overlapping buckets object 
+  const overlappingBuckets = {
+    "1995-12-17T09:00": [
       {
         name: 'testEvent-1y',
         location: 'india',
-        startsOn: {
-          date: 'June 30, 2021',
-          time: '09:00:00'
-        },
-        endsOn: {
-          date: 'June 30, 2021',
-          time: '10:00:00'
-        },
+        startsOn: "1995-12-17T09:00",
+        endsOn: "1995-12-17T10:00",
         isAllDay: true
       }
     ],
-    // "9:30": [
-
-    // ],
-    // "10": [
-
-    // ],
-    // "10:30": [
-
-    // ],
-    "11": [
+    "1995-12-17T11:00": [
       {
-        name: 'testEvent-2y',
+        name: 'testEvent-1y',
         location: 'india',
-        startsOn: {
-          date: 'June 30, 2021',
-          time: '11:00:00'
-        },
-        endsOn: {
-          date: 'June 30, 2021',
-          time: '12:30:00'
-        },
+        startsOn: "1995-12-17T11:00",
+        endsOn: "1995-12-17T12:30",
+        isAllDay: true
+      }],
+    "1995-12-17T13:00": [
+      {
+        name: 'testEvent-1y',
+        location: 'india',
+        startsOn: "1995-12-17T13:00",
+        endsOn: "1995-12-17T14:30",
+        isAllDay: true
+      },
+      {
+        name: 'testEvent-1y',
+        location: 'india',
+        startsOn: "1995-12-17T14:00",
+        endsOn: "1995-12-17T15:00",
+        isAllDay: true
+      }],
+    "1995-12-17T17:00": [
+      {
+        name: 'testEvent-1y',
+        location: 'india',
+        startsOn: "1995-12-17T17:00",
+        endsOn: "1995-12-17T20:00",
+        isAllDay: true
+      },
+      {
+        name: 'testEvent-1y',
+        location: 'india',
+        startsOn: "1995-12-17T17:30",
+        endsOn: "1995-12-17T19:00",
+        isAllDay: true
+      },
+      {
+        name: 'testEvent-1y',
+        location: 'india',
+        startsOn: "1995-12-17T17:00",
+        endsOn: "1995-12-17T17:30",
         isAllDay: true
       }
     ]
   }
+
+  const timeBlockEventStartMap = {}
+  //TODO: get largest event end time
+  let smallestEventStartTime = new Date("1995-12-17T09:00")
+  let largestEventEndTime = new Date("1995-12-17T20:00")
+  let lastTimeBlock = largestEventEndTime.getHours() - smallestEventStartTime.getHours()
+  console.log('lasttimeblock', (lastTimeBlock * 60) / 30) //splitting into blocks of 30 min each
+  let lastBlockIndex = (lastTimeBlock * 60) / 30
+  let i = 0;
+  let bucketStart = smallestEventStartTime
+  while (i < lastBlockIndex) {
+    if (getDateString(bucketStart) in overlappingBuckets) {
+      timeBlockEventStartMap[i] = { time: getDateString(bucketStart), hasEvents: true }
+    }
+    else {
+      timeBlockEventStartMap[i] = { time: getDateString(bucketStart), hasEvents: false }
+    }
+    let hrs = new Date(bucketStart).getMinutes()
+    bucketStart = new Date(bucketStart)
+    bucketStart.setMinutes(hrs + 30)
+    i++;
+  }
+  console.log("timeblockeventstartmap", timeBlockEventStartMap)
+
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
@@ -140,63 +131,74 @@ window.onload = function () {
   let smallestEventBlockHeight = hourBlockHeight / 2
   let smallestPossibleEventDurationInMinutes = 30
 
-  let overlappingEventCount = 3
   let gutter = 10
   let lineHeight = 10
   let timeMarkerFontSize = 25
 
   //paint time markers
-  for (timeBlock = 0; timeBlock < 24; timeBlock++) {
+  for (timeBlock = 0; timeBlock < lastBlockIndex; timeBlock++) {
     if (timeBlock % 12 == 0) {
       ctx.font = `${timeMarkerFontSize}px Arial`;
       ctx.fillStyle = 'black';
       ctx.fillText(`PM`, timePeriodMarkerOffset, (timeBlock * (hourBlockHeight)) + timeMarkerFontSize);
     }
-    let timeOffset = (timeBlock * (hourBlockHeight))
+    let timeOffset = (timeBlock * (smallestEventBlockHeight))
     //hour marker
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.moveTo(hourMarkerOffset, timeOffset);
-    ctx.lineTo(totalCanvasWidth, timeOffset);
-    ctx.strokeStyle = 'gray';
-    ctx.stroke();
+    if (!isHalfHour(timeBlockEventStartMap[timeBlock].time)) {
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.moveTo(hourMarkerOffset, timeOffset);
+      ctx.lineTo(totalCanvasWidth, timeOffset);
+      ctx.strokeStyle = 'gray';
+      ctx.stroke();
 
-    ctx.font = "10px Arial";
-    ctx.fillStyle = 'black';
-    ctx.fillText(`Time: ${timeBlock} PM`, hourMarkerOffset, timeOffset + lineHeight);
+      ctx.font = "10px Arial";
+      ctx.fillStyle = 'black';
+      ctx.fillText(`${timeBlockEventStartMap[timeBlock].time} PM`, hourMarkerOffset, timeOffset + lineHeight);
+    }
+    else {
+      //mid hour marker
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.moveTo(midHourMarkerOffset, timeOffset);
+      ctx.lineTo(totalCanvasWidth, timeOffset);
+      ctx.strokeStyle = 'gray';
+      ctx.stroke();
 
-    //mid hour marker
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.moveTo(midHourMarkerOffset, timeOffset + (hourBlockHeight / 2));
-    ctx.lineTo(totalCanvasWidth, timeOffset + (hourBlockHeight / 2));
-    ctx.strokeStyle = 'gray';
-    ctx.stroke();
-
-    ctx.font = "10px Arial";
-    ctx.fillStyle = 'black';
-    ctx.fillText(`Time: ${timeBlock}:30 PM`, midHourMarkerOffset, timeOffset + (hourBlockHeight / 2) + lineHeight);
+      ctx.font = "10px Arial";
+      ctx.fillStyle = 'black';
+      ctx.fillText(`${timeBlockEventStartMap[timeBlock].time} PM`, midHourMarkerOffset, timeOffset + lineHeight);
+    }
   }
 
-  for (timeBlock = 0; timeBlock < 10; timeBlock++) {
-    let timeOffset = (timeBlock * (hourBlockHeight)) // (timeBlock*(timeBlockSize + gutter))
+  for (timeBlock = 0; timeBlock < lastBlockIndex; timeBlock++) {
+    let timeOffset = (timeBlock * (smallestEventBlockHeight)) // (timeBlock*(timeBlockSize + gutter))
+    if (!timeBlockEventStartMap[timeBlock].hasEvents) {
+      continue
+    }
+    let blockTime = timeBlockEventStartMap[timeBlock].time
+    let events = overlappingBuckets[blockTime]
+    let overlappingEventCount = events.length
     let width = (eventAreaWidth / overlappingEventCount)
     for (eventBlock = 0; eventBlock < overlappingEventCount; eventBlock++) {
-      ctx.fillStyle = "white";// timeBlock %2 === 0 ? "#FF0000" : "blue"; 
+      ctx.fillStyle = "white";
 
-      let eventDurations = [30, 60, 90, 120]
-      let eventDuration = eventDurations[Math.floor(Math.random() * eventDurations.length)]
-      let eventDurationStartMins = [0, 30]
-      let eventDurationStartMin = eventDurationStartMins[Math.floor(Math.random() * eventDurationStartMins.length)]
+      let event = events[eventBlock]
+      let eventStart = new Date(event.startsOn)
+      let eventEnd = new Date(event.endsOn)
+      let eventDuration = getDiffInMins(eventStart, eventEnd);
+      
+      let eventOffsetMin = getDiffInMins(new Date(blockTime), eventStart)
+      // console.log(`ev offset ${eventOffset} for ev  for ${events[eventBlock]}`);
 
-      let eventBlockStart = eventDurationStartMin == 0 ? 0 : smallestEventBlockHeight
+      let eventBlockStart = eventOffsetMin == 0 ? 0 : (eventOffsetMin / smallestPossibleEventDurationInMinutes) * smallestEventBlockHeight //eventDurationStartMin == 0 ? 0 : smallestEventBlockHeight
       let eventBlockHeight = smallestEventBlockHeight * (eventDuration / smallestPossibleEventDurationInMinutes)
       let eventOffset = eventAreaOffset + eventBlock * (width + gutter)
 
       ctx.fillRect(eventOffset, timeOffset + eventBlockStart, width, eventBlockHeight);
 
       ctx.fillStyle = 'black';
-      ctx.fillText(`Event time: ${timeBlock} ${eventDurationStartMin} PM  duration ${eventDuration}`, eventOffset, timeOffset + eventBlockStart + lineHeight);
+      ctx.fillText(`Event time: ${event.startsOn} duration ${eventDuration} offset ${eventOffsetMin}`, eventOffset, timeOffset + eventBlockStart + lineHeight);
       ctx.fillText(`Event name: ${eventBlock}`, eventOffset, timeOffset + eventBlockStart + lineHeight + lineHeight);
       ctx.fillText(`Event Location: ${eventBlock}`, eventOffset, timeOffset + eventBlockStart + lineHeight + lineHeight + lineHeight);
 
@@ -210,6 +212,10 @@ window.onload = function () {
     }
   }
 
+  function isHalfHour(time) {
+    return new Date(time).getMinutes() === 30
+  }
+
   let graph = {}
   function addEdge(from, to) {
     if (from in graph) {
@@ -221,135 +227,77 @@ window.onload = function () {
   }
 
   //disconnected graph doesnt work
-  addEdge("1995-12-17T07:00", "1995-12-17T08:00")
-  addEdge("1995-12-17T07:30", "1995-12-17T09:00")
-  addEdge("1995-12-17T08:00", "1995-12-17T08:30")
-  addEdge("1995-12-17T09:00", "1995-12-17T09:30")
-  addEdge("1995-12-17T09:00", "1995-12-17T10:00")
-  addEdge("1995-12-17T09:00", "1995-12-17T10:30")
-  addEdge("1995-12-17T09:30", "1995-12-17T10:30")
-  addEdge("1995-12-17T09:30", "1995-12-17T11:00")
-  addEdge("1995-12-17T10:00", "1995-12-17T11:00")
-  addEdge("1995-12-17T10:30", "1995-12-17T11:30")
-  addEdge("1995-12-17T11:30", "1995-12-17T12:30")
-
+  // addEdge("1995-12-17T07:00", "1995-12-17T08:00")
+  // addEdge("1995-12-17T07:30", "1995-12-17T09:00")
+  // addEdge("1995-12-17T08:00", "1995-12-17T08:30")
+  // addEdge("1995-12-17T09:00", "1995-12-17T09:30")
   // addEdge("1995-12-17T09:00", "1995-12-17T10:00")
-  // addEdge("1995-12-17T11:00", "1995-12-17T12:30")
-  // addEdge("1995-12-17T13:00", "1995-12-17T14:30")
-  // addEdge("1995-12-17T14:00", "1995-12-17T15:00")
-  // addEdge("1995-12-17T17:00", "1995-12-17T20:00")
-  // addEdge("1995-12-17T17:30", "1995-12-17T19:00")
-  // addEdge("1995-12-17T17:00", "1995-12-17T17:30")
+  // addEdge("1995-12-17T09:00", "1995-12-17T10:30")
+  // addEdge("1995-12-17T09:30", "1995-12-17T10:30")
+  // addEdge("1995-12-17T09:30", "1995-12-17T11:00")
+  // addEdge("1995-12-17T10:00", "1995-12-17T11:00")
+  // addEdge("1995-12-17T10:30", "1995-12-17T11:30")
+  // addEdge("1995-12-17T11:30", "1995-12-17T12:30")
+
+  addEdge("1995-12-17T09:00", "1995-12-17T10:00")
+  addEdge("1995-12-17T11:00", "1995-12-17T12:30")
+  addEdge("1995-12-17T13:00", "1995-12-17T14:30")
+  addEdge("1995-12-17T14:00", "1995-12-17T15:00")
+  addEdge("1995-12-17T17:00", "1995-12-17T20:00")
+  addEdge("1995-12-17T17:30", "1995-12-17T19:00")
+  addEdge("1995-12-17T17:00", "1995-12-17T17:30")
 
   console.log('graph', graph)
 
-  // function getNonoverlappingEvents() {
-  //   let visited = {}
-  //   Object.keys(graph).forEach(node => {
-  //     visited[node] = false
-  //   })
-    
-  //   let eventStarts ={}
-  //   Object.keys(graph).forEach((eventStart)=>{
-  //     eventStarts[eventStart] = true
-  //   })
-
-  //   let largestKnownTime =  Object.keys(graph)[0]
-
-  //   Object.keys(graph).forEach((eventStart)=>{
-  //     console.log('--------------------event start', eventStart)
-  //     let queue = []
-  //     queue.push(eventStart)
-  //     // queue.push("1995-12-17T09:00")
-     
-  //     while (queue.length > 0) {
-  //       let node = queue.shift()
-  //       if (new Date(node) >= new Date(largestKnownTime)) {
-  //         console.log('non overlapping item', node)
-  //       }
-  //       // console.log('visiting', node)
-
-  //       if (node in graph) {
-
-  //         for (var child of graph[node]) {
-
-  //           if (!visited[child]) {
-  //             if (new Date(child) > new Date(largestKnownTime)) {
-  //               largestKnownTime = child
-  //               // console.log('------new largestKnownTime', largestKnownTime)
-  //             }
-  //             else{
-  //               // console.log(`${child} is less than largestKnownTime ${largestKnownTime}`)
-  //             }
-  //             if(child in eventStarts){
-  //               queue.push(child)
-  //             }
-  //             visited[child] = true
-  //           }
-  //         }
-  //       }
-  //     }
-  //   })
-
-  // }
-
   function getNonoverlappingEvents() {
-    
+
     let startDate = new Date("1995-12-17T07:00")
     let endDate = new Date("1995-12-17T17:30")
     let largestKnownTime = "1995-12-17T07:00"
     let nextBlock = startDate
     let timeBlocks = []
-    while(nextBlock < endDate){
-    
-
-      if(getDateString(nextBlock) in graph){
+    while (nextBlock < endDate) {
+      if (getDateString(nextBlock) in graph) {
         // console.log(`next block  found ${getDateString(nextBlock)}`)
         let eventStart = getDateString(nextBlock)
-        if(new Date(eventStart) >= new Date(largestKnownTime))
-        {
+        if (new Date(eventStart) >= new Date(largestKnownTime)) {
           console.log("non overlapping event", eventStart)
         }
         graph[eventStart].forEach((eventEnd) => {
-          if(new Date(eventEnd) > new Date(largestKnownTime)){
+          if (new Date(eventEnd) > new Date(largestKnownTime)) {
             largestKnownTime = eventEnd
             // console.log('------new largestKnownTime', largestKnownTime)
           }
-          else{
+          else {
             // console.log(`${eventEnd} is less than largestKnownTime ${largestKnownTime}`)
           }
         })
       }
-      else{
+      else {
         // console.log(`next block not found ${getDateString(nextBlock)}`)
       }
 
-      let hrs  = new Date(nextBlock).getMinutes()
+      let hrs = new Date(nextBlock).getMinutes()
       nextBlock = new Date(nextBlock)
-      nextBlock.setMinutes(hrs+30)
+      nextBlock.setMinutes(hrs + 30)
       // console.log(`next block is ${getDateString(nextBlock)}`)
     }
-    // Object.keys(graph).forEach((eventStart)=>{
-    //   if(eventStart >= largestKnownTime)
-    //   {
-    //     console.log("non overlapping event", eventStart)
-    //   }
-    //   graph[eventStart].forEach((eventEnd) => {
-    //     if(eventEnd > largestKnownTime){
-    //       largestKnownTime = eventEnd
-    //     }
-    //   })
-      
-    // })
   }
 
-  function getDateString(d){
+  function getDiffInMins(dateA, dateB) {
+    var diffMs = Math.abs(dateB - dateA); // milliseconds between now & Christmas
+    let result =  Math.floor((diffMs/1000)/60);
+    console.log(`diff bw ${dateA} & ${dateB} in min ${result}`)
+    return result
+  }
+
+  function getDateString(d) {
     var date = ("0" + d.getDate()).slice(-2)
-    var month =  ("0"+(d.getMonth()+1)).slice(-2)
-    var year =  d.getFullYear()
+    var month = ("0" + (d.getMonth() + 1)).slice(-2)
+    var year = d.getFullYear()
     var hrs = ("0" + d.getHours()).slice(-2)
     var min = ("0" + d.getMinutes()).slice(-2)
-    var datestring =year + "-" + month + "-" + date + "T" + hrs + ":" + min;
+    var datestring = year + "-" + month + "-" + date + "T" + hrs + ":" + min;
     return datestring;
   }
   getNonoverlappingEvents()
